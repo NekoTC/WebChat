d0fb57452c578f15a5a4d33587264237ad15ad75
NekoTC <xtwg17192522@outlook.com>
2024-12-21 22:05:44 +0800

    feature:add avatar

--- server.js
+++ server.js
@@ -13,47 +13,31 @@ const users = {};
 io.on('connection', (socket) => {
     console.log('用户已连接:', socket.id);
 
-    socket.on('setUsername', (username) => {
-        users[socket.id] = username;
-        console.log(`用户设置用户名: ${username} (${socket.id})`);
-        socket.emit('message', { sender: '服务器', text: `你的用户名已设置为 ${username}` });
-        socket.broadcast.emit('message', { sender: '服务器', text: `${username} 加入了聊天室！` });
+    socket.on('setUsername', (data) => {
+        const { username, email, avatar } = data;
+        users[socket.id] = { username, email, avatar };
+        console.log(`用户设置: ${username} (${socket.id}), 头像: ${avatar}`);
+
+        socket.emit('message', { sender: '服务器', text: `你的用户名已设置为 ${username}`, avatar });
+        socket.broadcast.emit('message', { sender: '服务器', text: `${username} 加入了聊天室！`, avatar });
         io.emit('updateUserList', Object.entries(users));
     });
 
     socket.on('chatMessage', (msg) => {
-        const username = users[socket.id] || '匿名用户';
-        console.log(`来自 ${username} (${socket.id}) 的消息:`, msg);
-        io.emit('message', { sender: username, text: msg });
-    });
-
-    socket.on('privateMessage', ({ targetId, message }) => {
-        const sender = users[socket.id] || '匿名用户';
-        if (users[targetId]) {
-            socket.to(targetId).emit('privateMessage', {
-                sender,
-                message,
-            });
-            console.log(`私聊消息: ${sender} -> ${users[targetId]}: ${message}`);
-        } else {
-            socket.emit('message', { sender: '服务器', text: '目标用户已离线或不存在。' });
-        }
-    });
-
-    socket.on('fetchOnlineUsers', () => {
-        socket.emit('updateUserList', Object.entries(users));
+        const user = users[socket.id] || { username: '匿名用户', avatar: '' };
+        console.log(`来自 ${user.username} (${socket.id}) 的消息:`, msg);
+        io.emit('message', { sender: user.username, text: msg, avatar: user.avatar });
     });
 
     socket.on('disconnect', () => {
-        const username = users[socket.id] || '匿名用户';
-        console.log('用户已断开连接:', username, socket.id);
-        io.emit('message', { sender: '服务器', text: `${username} 离开了聊天室。` });
+        const user = users[socket.id] || { username: '匿名用户', avatar: '' };
+        console.log('用户已断开连接:', user.username, socket.id);
+        io.emit('message', { sender: '服务器', text: `${user.username} 离开了聊天室。`, avatar: '' });
         delete users[socket.id];
         io.emit('updateUserList', Object.entries(users));
     });
 });
 
-// 启动服务器
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`服务器运行在 http://localhost:${PORT}`);
